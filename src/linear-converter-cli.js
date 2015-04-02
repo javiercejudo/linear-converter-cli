943433d84b6c52cf5f32e6b67360d87760fb9307
Javier Cejudo <javier@javiercejudo.com>
2015-04-03 03:08:28 +1100

    fix(exit): exit properly

--- src/linear-converter-cli.js
+++ src/linear-converter-cli.js
@@ -6,11 +6,11 @@ var program = require('commander');
 var lc = require('linear-converter');
 
 program
-  .version('0.0.1')
+  .version('0.0.5-alpha.1')
   .usage('<number> [options]')
-  .option('-n, --negative', 'Treat <number> as negative')
-  .option('-u, --unit [unit]', 'Unit for the preset')
-  .option('-p, --preset [preset]', 'Name of the preset to use')
+  .option('-n, --negative', 'treat <number> as negative')
+  .option('-u, --unit [unit]', 'unit for the preset')
+  .option('-p, --preset [preset]', 'name of the preset to use')
   .parse(process.argv);
 
 var x = Number(program.args[0]);
@@ -20,3 +20,5 @@ if (program.negative) {
 }
 
 console.log(lc.convert(x, lc.PRESETS[program.unit][program.preset]));
+
+process.exit(0);
