77af899b447c3b5deac8d7c70425cce6b35d4bb6
Javier Cejudo <javier@javiercejudo.com>
2015-04-09 02:15:28 +1000

    feat(options): inversion, basic composition

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ CLI for [linear-converter](https://github.com/javiercejudo/linear-converter)
 ## Basic usage
 
     linear-converter 25 -u temperature -p celsiusToFahrenheit
+
+## Help
+
+    Usage: linear-converter-cli <number> [options]
+
+    Options:
+
+      -h, --help                 output usage information
+      -V, --version              output the version number
+      -n, --negative             treat <number> as negative
+      -u, --unit [unit]          unit for the preset
+      -p, --preset [preset]      name of the preset to use
+      -i, --inverse              invert the preset
+      -1, --preset-one [preset]  first preset to compose (transparently inverted)
+      -2, --preset-two [preset]  second preset to compose
--- src/linear-converter-cli.js
+++ src/linear-converter-cli.js
@@ -11,14 +11,31 @@ program
   .option('-n, --negative', 'treat <number> as negative')
   .option('-u, --unit [unit]', 'unit for the preset')
   .option('-p, --preset [preset]', 'name of the preset to use')
+  .option('-i, --inverse', 'invert the preset')
+  .option('-1, --preset-one [preset]', 'first preset to compose (transparently inverted)')
+  .option('-2, --preset-two [preset]', 'second preset to compose')
   .parse(process.argv);
 
+var preset;
 var x = Number(program.args[0]);
 
 if (program.negative) {
   x = -x;
 }
 
-console.log(lc.convert(x, lc.PRESETS[program.unit][program.preset]));
+if (program.preset) {
+  preset = lc.PRESETS[program.unit][program.preset];
+} else if (program.presetOne.length > 0 && program.presetTwo.length > 0) {
+  preset = lc.composePresets([
+    lc.invertPreset(lc.PRESETS[program.unit][program.presetOne]),
+    lc.PRESETS[program.unit][program.presetTwo]
+  ]);
+}
+
+if (program.inverse) {
+  preset = lc.invertPreset(preset);
+}
+
+console.log(lc.convert(x, preset));
 
 process.exit(0);
