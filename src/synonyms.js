cae1079ecc9f2071535171cfa98d5601630d4f95
Javier Cejudo <javier@javiercejudo.com>
2015-10-19 19:43:08 +1100

    feat(synonyms): use synonyms package for luminous intensity

--- src/synonyms.js
+++ src/synonyms.js
@@ -14,6 +14,7 @@ var synonyms = {
   area: require('unit-synonyms-area').synonyms,
   electricCurrent: require('unit-synonyms-electric-current').synonyms,
   length: require('unit-synonyms-length').synonyms,
+  luminousIntensity: require('unit-synonyms-luminous-intensity').synonyms,
   mass: require('unit-synonyms-mass').synonyms,
   metricPrefixes: require('unit-synonyms-metric-prefixes').synonyms,
   temperature: require('unit-synonyms-temperature').synonyms,
@@ -27,7 +28,6 @@ synonyms.distance = synonyms.length;
 module.exports = (presets) => {
   var missingProperties = [
     'digitalInformation',
-    'luminousIntensity',
     'velocity'
   ];
 
